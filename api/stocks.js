0f273aa4cb9405aa6495452b55fb20b4afb94c7c
Chan Kim <104743574+wannabechan@users.noreply.github.com>
2026-01-19 09:29:18 +0900

    Add files via upload

--- api/stocks.js
+++ api/stocks.js
@@ -2,7 +2,7 @@
 // 경로: /api/stocks?codes=005930,000660,005380
 
 import axios from 'axios';
-import { getAccessToken, getTodayString, getStockName, APP_KEY, APP_SECRET } from './_shared/kis-api.js';
+import { getAccessToken, getTodayString, getStockName, getCurrentPrice, APP_KEY, APP_SECRET } from './_shared/kis-api.js';
 
 // 환경변수에서 API 키 가져오기
 const KIS_APP_KEY = process.env.KIS_APP_KEY || APP_KEY;
@@ -62,6 +62,9 @@ export default async function handler(req, res) {
       try {
         // 종목명 가져오기
         const stockName = await getStockName(stockCode, accessToken, KIS_APP_KEY, KIS_APP_SECRET);
+        
+        // 현재가 가져오기
+        const currentPrice = await getCurrentPrice(stockCode, accessToken, KIS_APP_KEY, KIS_APP_SECRET);
         
         // 한국투자증권 일자별 시세 조회 API (타임아웃 및 재시도 포함)
         let response;
@@ -148,6 +151,7 @@ export default async function handler(req, res) {
         
         results[stockCode] = {
           name: stockName,
+          currentPrice: currentPrice, // 현재가 추가
           // 최근 개장일 바로 이전의 개장일 정보
           prevDate: date,
           prevOpen: parseInt(data.stck_oprc) || 0,
